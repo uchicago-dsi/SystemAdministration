3ea9b903eddeef38322c7ff9adafcb1b3fce5aa6
Maria Hernandez <mehernandez@fe01.ds.uchicago.edu>
2025-03-11 10:48:36 -0500

    Creating Script to ensure directories inherit their parent group's permissions

---
+++ chGroupScript.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Description: Script to ensure directories inherit their parent group's permissions.
+# Scheduled to run every hour.
+
+PathProject1="/net/projects"
+PathProject2="/net/projects2"
+
+for dir in $PathProject1; do
+	group_name=$(ls -ld "$dir" | awk '{print $4}')
+	chgrp -R "$group_name" "$dir" 
+	chmod -R g+s "$dir"
+done
+
+for dir in $PathProject2; do
+	group_name=$(ls -ld "$dir" | awk '{print $4}') 
+        chgrp -R "$group_name" "$dir" 
+        chmod -R g+s "$dir"
+done
+
