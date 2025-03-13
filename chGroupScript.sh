5f581a330e5f2e1fa553db9c1c239e29ea740a6b
root <root@cluster-storage1.ds.uchicago.edu>
2025-03-13 16:07:02 -0500

    Requested changes have been implemented in the Modif-chGroup branch

--- chGroupScript.sh
+++ chGroupScript.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-# Description: Script to ensure directories inherit their parent group's permissions.
-<<<<<<< Updated upstream
-# Scheduled to run every hour.
-=======
-# Scheduled to run every day.
->>>>>>> Stashed changes
+#"Author: Maria Hernandez
+#Email: mehernandez@uchicago.edu"
 
 # Define project paths
 PathProject1="/tank/projects"
@@ -16,11 +12,10 @@ HostnameCluster1="cluster-storage1"
 HostnameCluster2="cluster-storage4"
 
 # Define directories to exclude
-ChachaDir="chacha"
 ChailabDir="chai-lab"
 
 #Getting the hostname of the server
-hostname=$(hostname | awk '{print $1}')
+hostname=$(hostname)
 
 #Checking which server the script will be running
 #Depending of the server, the pasth changes
@@ -35,16 +30,16 @@ LogFile="/var/log/chGroupScript_$(date '+%d-%b-%Y').log"
 touch "$LogFile"
 
 #Saving the date that the script is being executed
-echo "Starting script execution at $date" >> "$LogFile"
+echo "Starting script execution at $(date)" >> "$LogFile"
 
 #Going through each directory inside of the projects/projects2 directory
 #Exclude directories matching "chacha" or "chai-lab"
 for dir in "$Path"/*/; do
 
-	if [[ "$dir" != "$ChachaDir" && "$dir" != "$ChailabDir" ]]; then  
+	if [[ "$dir" != "$ChailabDir" ]]; then
 		echo "Processing directory: $dir at $(date | awk '{printf "%s\n", $4}')" >> "$LogFile"
 		group_name=$(ls -ld "$dir" | awk '{print $4}')
-        	chgrp -R "$group_name" "$dir" 
+        	chgrp -R "$group_name" "$dir"
         	chmod -R g+s "$dir"
 		echo "Finished processing: $dir (Group: $group_name) at $(date | awk '{printf "%s\n", $4}')" >> "$LogFile"
 	fi
