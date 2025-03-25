9e965b155c04424b154776bf71b94b26ff40b4c1
mefigeniah <mehernandez@uchicago.edu>
2025-03-25 14:41:26 -0500

    Prevent overwriting quota configurations

--- setMemScript.sh
+++ setMemScript.sh
@@ -1,58 +1,53 @@
 #!/bin/bash
 
-#Script to set a 50GB memory limit per user in the scratch and scratch2 directories
+#"Author: Maria Hernandez
+#Email: mehernandez@uchicago.edu"
 
-# Define cluster hostnames
-ClusterScratch1="cluster-storage2"
-ClusterScratch2="cluster-storage4"
+#Description: Script to set a 50GB memory limit per user in the scratch and scratch2 directories
+# Scheduled to run every day.
 
-# Define scracth paths
-PathScratch1="/tank/scratch"
-PathScratch2="/tank/scratch2"
+#Define the name of the script
+script="setMemScript"
+
+#Define servers_path.txt path
+servers_paths="/root/SystemAdministration/server_paths.txt"
+
+#Getting the hostname of the server
+hostname=$(hostname)
+
+#Determine the server where the script is running
+#Set the directory path based on the server
+#server names and path directories: servers_paths.txt
+Path=$(grep "^$hostname $script" "$servers_paths" | awk '{print $3}')
+
+#If the hostname is not on the list, exit the code
+if [ -z "$Path" ]; then
+  exit 0
+fi
 
 #Define the files
 LogDir="/var/log"
 ZFSData="$LogDir/zfsListUsers.txt"
-LoginData="$LogDir/loginListUsers.txt"
+LoginDataFe01="$LogDir/loginListUsersFe01.txt"
+LoginDataFe02="$LogDir/loginListUsersFe02.txt"
 LlData="$LogDir/llData.txt"
-AllUsers="$LogDir/allUsers.txt"
 
 #Test Files
 ZFSDataLog="$LogDir/zfsListUsersLog.txt"
-LoginDataLog="$LogDir/loginListUsersLog.txt"
+LoginDataLogFe01="$LogDir/loginListUsersLogFe01.txt"
+LoginDataLogFe02="$LogDir/loginListUsersLogFe02.txt"
 LlDataLog="$LogDir/llDataLog.txt"
 touch "$ZFSDataLog"
-touch "$LoginDataLog"
+touch "$LoginDataLogFe01"
+touch "$LoginDataLogFe02"
 touch "$LlDataLog"
-touch "$AllUsers"
 
 
 #Making sure the files exist
 touch "$ZFSData"
 touch "$LlData"
 
-
-#Getting the hostname of the server
-hostname=$(hostname)
-
-#Checking which server the script will be running
-#Depending of the server, the path changes
-if [ "$hostname" = "$ClusterScratch1" ]; then
-	Path="$PathScratch1"
-elif [ "$hostname" = "$ClusterScratch2" ]; then
-	Path="$PathScratch2"
-else
-  #If the hostname is not listed among the defined clusters in line 14, the script will exit without displaying an error message
-	  exit 0;
-fi
-
-# If the file is empty, add the header
-if [ ! -s "$AllUsers" ]; then
-  echo "Users with a 50GB memory limit in the scratch and scratch2 directories." > "$AllUsers"
-  echo "" >> "$AllUsers"
-fi
-
-#Getting all the users by zfs
+#Getting all the users with their quota updated
 zfs userspace "$Path" | tail -n +2 | awk '{print $3}' | sort > "$ZFSData"
 
 #Getting all the users from a long list command in the Scratch/Starch2 directory
@@ -60,41 +55,34 @@ ls -la "$Path" | awk '{print $3}' > "$LlData"
 
 newPath="${Path:1}"
 
-# Loop through each user in the file
+# Loop through each user in the file LoginDataFe01
 while read -r user; do
-  zfs set userquota@"$user"=50G "$newPath"
-  echo "$user" >> "$ZFSDataLog"
-
-  #check if the user is in the All user files.
-  if ! grep -qE "^zfs set userquota@ $user =50G $newPath$" "$AllUsers"; then
-        echo "zfs set userquota@ $user =50G $newPath" >> "$AllUsers"
-  fi
-
-done < "$ZFSData"
-
-# Loop through each user in the file
-while read -r user; do
-  zfs set userquota@"$user"=50G "$newPath"
-  echo "$user" >> "$LoginDataLog"
-
-  #check if the user is in the All user files.
-  if ! grep -qE "^zfs set userquota@ $user =50G $newPath$" "$AllUsers"; then
-      echo "zfs set userquota@ $user =50G $newPath" >> "$AllUsers"
-  fi
-
-
-done < "$LoginData"
-
-# Loop through each user in the file
-while read -r user; do
-  zfs set userquota@"$user"=50G "$newPath"
-  echo "$user" >> "$LlDataLog"
-
-  #check if the user is in the All user files.
-    if ! grep -qE "^zfs set userquota@ $user =50G $newPath$" "$AllUsers"; then
-          echo "zfs set userquota@ $user =50G $newPath" >> "$AllUsers"
+  #Check if the user has a quota already set
+  #set the quota in case the user does not have one
+  if ! grep -qE "$user" "$ZFSData"; then
+        #zfs set userquota@"$user"=50G "$newPath"
+        echo "this $user is new, setting quota to 50G" >> "$LoginDataLogFe01"
     fi
+done < "$LoginDataFe01"
 
+# Loop through each user in the file LoginDataFe02
+while read -r user; do
+  #Check if the user has a quota already set
+  #set the quota in case the user does not have one
+  if ! grep -qE "$user" "$ZFSData"; then
+        #zfs set userquota@"$user"=50G "$newPath"
+        echo "this $user is new, setting quota to 50G" >> "$LoginDataLogFe02"
+    fi
+done < "$LoginDataFe02"
+
+# Loop through each user in the file LlData
+while read -r user; do
+  #Check if the user has a quota already set
+    #set the quota in case the user does not have one
+    if ! grep -qE "$user" "$ZFSData"; then
+          #zfs set userquota@"$user"=50G "$newPath"
+          echo "this $user is new, setting quota to 50G" >> "$LlDataLog"
+      fi
 done < "$LlData"
 
 
