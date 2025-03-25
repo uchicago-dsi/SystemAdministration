6294496b97f37b400877a3f97f0f43119b8ee8e7
mefigeniah <mehernandez@uchicago.edu>
2025-03-25 14:40:54 -0500

    Change the if loop to use a document list for checking the server name

--- Scripts/chGroupScript.sh
+++ Scripts/chGroupScript.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-#"Author: Maria Hernandez
+#Author: Maria Hernandez
 #Email: mehernandez@uchicago.edu"
 
-# Define project paths
-PathProject1="/tank/projects"
-PathProject2="/tank/projects2"
+# Description: Change the group ownership of directories within the file system to the correct group that belongs
+# Scheduled to run every Monday.
 
-# Define cluster hostnames
-HostnameCluster1="cluster-storage1"
-HostnameCluster2="cluster-storage4"
+#Define the name of the script
+script="chGroupScript"
+
+#Define servers_path.txt path
+servers_paths="/root/SystemAdministration/server_paths.txt"
 
 # Define directories to exclude
 ChailabDir="chai-lab"
@@ -17,14 +18,13 @@ ChailabDir="chai-lab"
 #Getting the hostname of the server
 hostname=$(hostname)
 
-#Checking which server the script will be running
-#Depending of the server, the path changes
-if [ "$hostname" = "$HostnameCluster1" ]; then
-	Path="$PathProject1"
-elif [ "$hostname" = "$HostnameCluster2" ]; then
-	Path="$PathProject2"
-else
-  #If the hostname is not listed among the defined clusters in line 14, the script will exit without displaying an error message
+#Determine the server where the script is running
+#Set the directory path based on the server
+#server names and path directories: servers_paths.txt
+Path=$(grep "^$hostname $script" "$servers_paths" | awk '{print $3}')
+
+#If the hostname is not on the list, exit the code
+if [ -z "$Path" ]; then
   exit 0
 fi
 
